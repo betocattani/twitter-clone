df8e348bed84982730ae608d2e2e6e66b7f1a188
Cattani <contato.cattani@gmail.com>
2021-03-20 17:09:32 -0300

    increment likes and reposts using broadcast

--- lib/chirp_web/live/post_live/post_component.ex
+++ lib/chirp_web/live/post_live/post_component.ex
@@ -18,11 +18,13 @@ defmodule ChirpWeb.PostLive.PostComponent do
       <div class="row">
         <div class="column">
           <a href="#" phx-click="like" phx-target="<%= @myself %>">
-          <i class="far fa-heart"></i> <%= @post.likes_count %>
+            <i class="far fa-heart"></i> <%= @post.likes_count %>
+          </a>
         </div>
         <div class="column">
           <a href="#" phx-click="repost" phx-target="<%= @myself %>">
-          <i class="far fa-hand-peace"></i> <%= @post.reposts_count %>
+            <i class="far fa-hand-peace"></i> <%= @post.reposts_count %>
+          </a>
         </div>
         <div class="column">
           <%= live_patch to: Routes.post_index_path(@socket, :edit, @post.id) do %>
@@ -35,5 +37,15 @@ defmodule ChirpWeb.PostLive.PostComponent do
       </div>
     </div>
     """
+  end
+
+  def handle_event("like", _, socket) do
+    Chirp.Timeline.inc_likes(socket.assigns.post)
+    {:noreply, socket}
+  end
+
+  def handle_event("repost", _, socket) do
+    Chirp.Timeline.inc_reposts(socket.assigns.post)
+    {:noreply, socket}
   end
 end
